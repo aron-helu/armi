8c3b9811951535b81802eb0d3de04a48b03c6b48
aron-helu <aronmilahela@gmail.com>
2023-07-14 23:19:38 +0300

    Creating the home page

--- src/app/Carousel.js
+++ src/app/Carousel.js
@@ -36,10 +36,12 @@ const Hero = () => {
                             <div className={`${styles.container} ${styles.responsiveContainer}`}>
                                 <div className="d-flex flex-column align-items-center justify-content-center h-100">
                                     <div className="p-3" style={{ maxWidth: '700px' }}>
+                                        <hr className="p-2 border-solid border-2 border-yellow-500 bg-yellow-700" />
                                         <h6 className="section-title text-white text-uppercase mb-3 animated slideInDown">Vacation Getaway</h6>
+                                        <hr className="p-2" />
                                         <h1 className="display-3 text-white mb-4 animated slideInDown">Discover Vacation Getaway Guest House</h1>
                                         <p className="lead text-white mb-4 animated slideInDown">
-                                            Our guest house is the perfect place to relax and unwind on your next vacation. We offer a variety of vacation amenities, including a kitchenette, a TV.
+                                            Our guest house is the perfect place to relax and unwind on your next vacation. We offer a variety of vacation amenities, including a kitchen and a TV.
                                         </p>
                                         <a href="" className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Learn More</a>
                                         <a href="" className="btn btn-light py-md-3 px-md-5 animated slideInRight">Book A Room</a>
