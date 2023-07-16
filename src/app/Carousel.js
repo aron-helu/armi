e242d12b7c8d545c32d4f7657d5db9f0cec614bc
aron-helu <aronmilahela@gmail.com>
2023-07-17 02:51:24 +0300

    Creating meta data

--- src/app/Carousel.js
+++ src/app/Carousel.js
@@ -25,14 +25,19 @@ const Hero = () => {
                             <div className={`${styles.container} ${styles.responsiveContainer}`}>
                                 <div className="d-flex flex-column align-items-center justify-content-center h-100">
                                     <div className="p-3" style={{ maxWidth: '700px' }}>
-                                        
                                         <h6 className={`section-title text-white text-uppercase mb-3 animated slideInDown`}>
-                                          Vacation Getaway
+                                            Your Private Oasis
                                         </h6>
-                                        <h1 className="display-3 text-white mb-4 animated slideInDown">Discover Vacation Getaway Guest House</h1>
-                                        <p className="lead text-white mb-4 animated slideInUp">Our guest house is the perfect place to relax and unwind on your next vacation. We offer a variety of vacation amenities, including a kitchen and a TV.</p>
-                                        <a href="" className="btn btn-primary py-md-3 px-md-5 me-3" style={{ backgroundColor: "#FEA116", color: "black" }}>Learn More</a>
-                                        <a href="" className="btn btn-light py-md-3 px-md-5 animated slideInUp">Book A Room</a>
+                                        <h1 className="display-3 text-white mb-4 animated slideInDown">Discover the Ultimate Getaway</h1>
+                                        <p className="lead text-white mb-4 animated slideInUp">
+                                            Uncover a little piece of paradise at our guest house in Asmara, Eritrea. Our cozy rooms offer comfortable beds, invigorating showers, and all the amenities you need for a memorable stay. Experience pure relaxation and create lasting memories on your next vacation. Reserve your retreat now!
+                                        </p>
+                                        <a href="" className="btn btn-primary py-md-3 px-md-5 me-3" style={{ backgroundColor: "#FEA116", color: "black" }}>
+                                            Learn More
+                                        </a>
+                                        <a href="" className="btn btn-light py-md-3 px-md-5 animated slideInUp">
+                                            Reserve Now
+                                        </a>
                                     </div>
                                 </div>
                             </div>
